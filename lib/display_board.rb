e44e81716c2f0fde20da4da0672b7578ef4a9d9f
Justin Breneman <justin.breneman@yahoo.com>
2020-04-04 00:06:18 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,6 +1,6 @@
 # Define display_board that accepts a board and prints
 # out the current state.
-rows = ["   |   |   ", "-----------"]
+rows = [" #{board[0]} | #{board[1]} | #{board[2]} ", "-----------"]
 
 def display_board(rows)
   puts rows[0]
