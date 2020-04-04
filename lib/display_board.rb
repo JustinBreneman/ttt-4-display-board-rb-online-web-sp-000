be45f8fa322005aa6a3a17ebb039ee35b3657760
Justin Breneman <justin.breneman@yahoo.com>
2020-04-04 00:14:34 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,6 +1,10 @@
 # Define display_board that accepts a board and prints
 # out the current state.
 
-def display_board(rows)
-  puts rows
+def display_board(board)
+  puts "   |   |   "
+  puts "-----------"
+  puts "   |   |   "
+  puts "-----------"
+  puts "   |   |   "
 end
